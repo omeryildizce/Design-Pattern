604e020b399b64fad54ea1db4770266c54775a4b
omeryildizce <omeryildizce@gmail.com>
2023-03-19 16:37:10 +0300

    Create TelefonFabrikasi.java

---
+++ DesignPattern/src/designpatterns/factory/TelefonFabrikasi.java
@@ -0,0 +1,18 @@
+package designpatterns.factory;
+
+public class TelefonFabrikasi {
+    public static Telefon getTelefon(String model, String batarya, int en, int boy){
+        Telefon telefon;
+        if ("s8".equalsIgnoreCase(model)){
+                telefon = new S8(model, batarya, en, boy);
+
+        }else if ("note8".equalsIgnoreCase(model)){
+            telefon = new Note8(model, batarya, en, boy);
+        }else {
+            throw new RuntimeException("Geçerli bir model değildir!");
+
+        }
+
+        return telefon;
+    }
+}
