42975e4f60101971c8d59300658c4cc9576a7516
omeryildizce <omeryildizce@gmail.com>
2023-03-19 16:37:01 +0300

    Create Note8.java

---
+++ DesignPattern/src/designpatterns/factory/Note8.java
@@ -0,0 +1,45 @@
+package designpatterns.factory;
+
+public class Note8 implements Telefon {
+    private String model;
+    private String batarya;
+    private int en;
+    private int boy;
+
+    public Note8(String model, String batarya, int en, int boy) {
+        this.model = model;
+        this.batarya = batarya;
+        this.en = en;
+        this.boy = boy;
+    }
+
+    @Override
+    public String toString() {
+        return "Note8{" +
+                "model='" + model + '\'' +
+                ", batarya='" + batarya + '\'' +
+                ", en=" + en +
+                ", boy=" + boy +
+                '}';
+    }
+
+    @Override
+    public String getModel() {
+        return null;
+    }
+
+    @Override
+    public String getBatarya() {
+        return null;
+    }
+
+    @Override
+    public int getEn() {
+        return 0;
+    }
+
+    @Override
+    public int getBoy() {
+        return 0;
+    }
+}
