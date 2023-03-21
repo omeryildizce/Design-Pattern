88b8979150e27dba02d9dc0c34c46129c737c83b
omeryildizce <omeryildizce@gmail.com>
2023-03-21 20:09:47 +0300

    Create EmlakciV2.java

---
+++ DesignPattern/src/designpatterns/builder/EmlakciV2.java
@@ -0,0 +1,32 @@
+package designpatterns.builder;
+
+public class EmlakciV2 {
+    public static void main(String[] args) {
+        EvBuilder evBuilder = EvBuilder.startBuild("istanbul")
+
+                .setIlçe("Ümraniye")
+                .setMahalle("Tatlısu")
+                .setBinaYili(2008)
+                .setOdaSayisi(3)
+                .setHasCocukParki(true)
+                .setHasotopark(true)
+                ;
+
+        Ev ev = EvBuilder.startBuild("istanbul")
+
+                .setIlçe("Ümraniye")
+                .setMahalle("Tatlısu")
+                .setBinaYili(2008)
+                .setOdaSayisi(3)
+                .setHasCocukParki(true)
+                .setHasotopark(true)
+                .buid();
+        printEv(ev);
+
+    }
+
+    private static void printEv(Ev ev) {
+        System.out.println();
+        System.out.println("Ev eklendi -> " + ev.toString());
+    }
+}
