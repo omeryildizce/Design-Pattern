6998f90de0902587a33afa54a41f22f93a9ee5f4
omeryildizce <omeryildizce@gmail.com>
2023-03-23 20:53:14 +0300

    Create SamsungTelefon.java

---
+++ DesignPattern/src/designpatterns/strucrual/adapter/SamsungTelefon.java
@@ -0,0 +1,15 @@
+package designpatterns.strucrual.adapter;
+
+public class SamsungTelefon implements Telefon{
+    private int calismaVoltaji;
+
+    public SamsungTelefon() {
+        this.calismaVoltaji = 5;
+    }
+
+    @Override
+    public int sarjEt() {
+        System.out.println("Samsung Telefon çalışıyor.");
+        return calismaVoltaji;
+    }
+}
