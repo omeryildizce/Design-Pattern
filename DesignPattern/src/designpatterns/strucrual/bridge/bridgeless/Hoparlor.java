1a62679e4460ef509e4aa14e428392b1e762c7e0
omeryildizce <omeryildizce@gmail.com>
2023-04-08 12:13:04 +0300

    Create Hoparlor.java

---
+++ DesignPattern/src/designpatterns/strucrual/bridge/bridgeless/Hoparlor.java
@@ -0,0 +1,7 @@
+package designpatterns.strucrual.bridge.bridgeless;
+
+public class Hoparlor {
+    public void sesiCal(String ses){
+        System.out.printf("Hoparlör ses veriyor %s%n", ses);
+    }
+}
