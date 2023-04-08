23339e13e4a7a04eb38a05147ee4f892669c120b
omeryildizce <omeryildizce@gmail.com>
2023-04-08 12:12:44 +0300

    Create MuzikCalar.java

---
+++ DesignPattern/src/designpatterns/strucrual/bridge/bridge/muzikcalar/MuzikCalar.java
@@ -0,0 +1,7 @@
+package designpatterns.strucrual.bridge.bridge.muzikcalar;
+
+import designpatterns.strucrual.bridge.bridge.Muzik;
+
+public interface MuzikCalar {
+     String muzikCal(Muzik muzik);
+}
