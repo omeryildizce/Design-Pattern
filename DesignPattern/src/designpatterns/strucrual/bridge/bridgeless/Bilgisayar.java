fc01fa6c86ce20ce4a1b878ddd789fce0347083f
omeryildizce <omeryildizce@gmail.com>
2023-04-08 12:13:09 +0300

    Create Bilgisayar.java

---
+++ DesignPattern/src/designpatterns/strucrual/bridge/bridgeless/Bilgisayar.java
@@ -0,0 +1,32 @@
+package designpatterns.strucrual.bridge.bridgeless;
+
+public class Bilgisayar {
+    private Spotify spotify;
+    private Hoparlor hoparlor;
+
+    public Bilgisayar() {
+        spotify = new Spotify();
+        hoparlor = new Hoparlor();
+
+    }
+    public void muzikCal(Muzik muzik){
+        System.out.println("Bilgisayar çalıştı");
+        String ses = spotify.muzikCalar(muzik);
+        hoparlor.sesiCal(ses);
+    }
+    public Spotify getSpotify() {
+        return spotify;
+    }
+
+    public void setSpotify(Spotify spotify) {
+        this.spotify = spotify;
+    }
+
+    public Hoparlor getHoparlor() {
+        return hoparlor;
+    }
+
+    public void setHoparlor(Hoparlor hoparlor) {
+        this.hoparlor = hoparlor;
+    }
+}
